1356883e20fa8ec63e55b4e60a24920e69406381
yussufsamir <yussufsamir18@gmail.com>
2025-05-25 15:33:30 -0700

    Added basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+r1=number1+number2
+r2=number1-number2
+r3=number1*number2
+print(f"sum of {number1} and {number2} is {r1}")
+print(f"difference of {number1} and {number2} is {r2}")
+print(f"product of {number1} and {number2} is {r3}")
