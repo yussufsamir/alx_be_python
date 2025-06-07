d0a76a401db6edb8478c09426ea28eca2a21448d
yussufsamir <yussufsamir18@gmail.com>
2025-06-07 03:48:46 -0700

    added task 0

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,25 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        return num1 / num2
+    else:
+        print("Invalid operation. Choose from 'add', 'subtract', 'multiply', 'divide'.")
+
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
