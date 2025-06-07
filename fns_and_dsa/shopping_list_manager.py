dbaacc96eaaa155daf89ef00917ed7f8a59a8f5d
yussufsamir <yussufsamir18@gmail.com>
2025-06-07 03:57:37 -0700

    task1

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,39 @@
+
+
+
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            x=input("Enter the item to add: ")
+            shopping_list.append(x)
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            y=input("Enter the item to remove: ")
+            shopping_list.remove(y)
+            pass
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
