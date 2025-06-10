cd9bf8c74061a7c457d20691a247180414189c47
yussufsamir <yussufsamir18@gmail.com>
2025-06-10 15:27:26 -0700

    added task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.balance:
+            self.balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current balance: {self.balance}")
+
