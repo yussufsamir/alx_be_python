767ab23f5da02ec66aac5def84002cd7d123915c
yussufsamir <yussufsamir18@gmail.com>
2025-06-14 06:02:13 -0700

    fix

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,8 +1,27 @@
 class Book:
     def __init__(self, title, author):
-        self.title = title
-        self.author = author
+        
+        self.title = title  
+        self.author = author  
         self._is_checked_out = False
+
+    def check_out(self):
+        
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return f'"{self.title}" has been checked out.'
+        return f'"{self.title}" is already checked out.'
+
+    def return_book(self):
+        
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return f'"{self.title}" has been returned.'
+        return f'"{self.title}" was not checked out.'
+
+    def is_available(self):
+        
+        return not self._is_checked_out
 
 class Library:
     def __init__(self):
